43e802b7a19dd147826dce3d17fdf40d2143bc23
Arina <ahmerkulova@gmail.com>
2021-12-14 13:37:42 +0300

    changed name for skill method, simplified offer method

--- Python_module/Homework_2/task01.py
+++ Python_module/Homework_2/task01.py
@@ -4,44 +4,42 @@ class Student:
         self.last_name = last_name
         self.age = age
         self.skills = ['English']
-        self.results = ['Passed']
         self.is_learning = True
 
-    def got_skill(self, skill_name, result):
+    def learn_skill(self, skill_name):
         self.skills.append(skill_name)
-        self.results.append(result)
 
-    def got_offer(self):
-        self.counter = 0
-        for i in range(len(self.results)):
-            if self.results[i] in 'Passed':
-                self.counter += 1
-        if self.counter == 4:
-            return f'{self.counter} of {len(self.results)} - congrats, you got the offer!'
+    def get_offer(self):
+        self.skills_needed = 0
+        for i in range(len(self.skills)):
+            if self.skills[i] in ['SQL', 'Python', 'Linux', 'English']:
+                self.skills_needed += 1
+        if self.skills_needed != 4:
+            return 'Sorry, you got no offer!'
         else:
-            return f'{self.counter} of {len(self.results)} - sorry, you got no offer!'
+            return 'Congrats, you got the offer!'
 
-    def result_info(self):
+    def print_result(self):
         print(f'Student name {self.first_name} {self.last_name}, age {self.age}:')
         for i in range(len(self.skills)):
-            print(f'\t{self.skills[i]} result: {self.results[i]}')
-        print(f'{self.got_offer()}\n')
+            print(self.skills[i])
+        print(f'{self.get_offer()}\n')
+
 
 john = Student('John', 'Smith', 17)
-john.got_skill('Linux', 'Passed')
-john.got_skill('SQL', 'Passed')
-john.got_skill('Python', 'Passed')
+john.learn_skill('Linux')
+john.learn_skill('SQL')
+john.learn_skill('Python')
 
 mary = Student('Bloody', 'Mary', 45)
-mary.got_skill('Linux', 'Passed')
-mary.got_skill('SQL', 'Failed')
-mary.got_skill('Python', 'Failed')
+mary.learn_skill('Linux')
+mary.learn_skill('SQL')
+mary.learn_skill('Python')
 
 peter = Student('Peter', 'Parker', 31)
-peter.got_skill('Linux', 'Passed')
-peter.got_skill('SQL', 'Passed')
-peter.got_skill('Python', 'Passed')
+peter.learn_skill('Linux')
+peter.learn_skill('SQL')
 
-john.result_info()
-mary.result_info()
-peter.result_info()
+john.print_result()
+mary.print_result()
+peter.print_result()
