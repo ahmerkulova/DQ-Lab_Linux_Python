843bbd520880de5f5b10357eba4022d6bee2cadc
Arina <ahmerkulova@gmail.com>
2021-12-15 16:59:47 +0300

    added avg_price for child classes

--- Python_module/Homework_2/task02.py
+++ Python_module/Homework_2/task02.py
@@ -1,47 +1,55 @@
 class Item:
     item_list = []
 
-    def __init__(self, category, title, price):
-        self.category = category
-        self.title = title
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
         self.price = price
-        self.item_list.append(price)
+        __class__.item_list.append(price)
 
-    def avg_price():
-        return round(sum(Item.item_list) / len(Item.item_list), 2)
+    def avg_price(__class__):
+        return round(sum(__class__.item_list) / len(__class__.item_list), 2)
 
 
 class Tablet(Item):
+    item_list = []
 
-    def __init__(self, category, title, price, display):
-        super().__init__(category, title, price)
+    def __init__(self, brand, model, price, display):
+        super().__init__(brand, model, price)
         self.display = display
+        __class__.item_list.append(price)
 
     def __str__(self):
-        return f'Category: {self.category}, Title: {self.title}, Price: {self.price}, Display: {self.display} inches'
+        return f'Brand: {self.brand}, Model: {self.model}, Price: {self.price}, Display: {self.display} inches'
 
 
 class Fridge(Item):
+    item_list = []
 
-    def __init__(self, category, title, price, capacity):
-        super().__init__(category, title, price)
+    def __init__(self, category, model, price, capacity):
+        super().__init__(category, model, price)
         self.capacity = capacity
+        __class__.item_list.append(price)
 
     def __str__(self):
-        return f'Category: {self.category}, Title: {self.title}, Price: {self.price}, Capacity: {self.capacity} liters'
+        return f'Brand: {self.brand}, Model: {self.model}, Price: {self.price}, Capacity: {self.capacity} liters'
 
 
-ipad = Tablet('Tablets', 'iPad pro', 1899, 11)
-ipad_mini = Tablet('Tablets', 'iPad mini', 999, 8)
-ariston = Fridge('Dual-chamber fridges', 'Hotpoint Ariston', 1899, 350)
-smeg = Fridge('Single-chamber fridges', 'Smeg', 2100, 350)
+ipad = Tablet('Apple', 'iPad pro', 1899, 11)
+ipad_mini = Tablet('Apple', 'iPad mini', 999, 8)
+ariston = Fridge('Hotpoint Ariston', 'HS 3200', 1899, 350)
+smeg = Fridge('Smeg', 'FAB28LPB5', 2200, 350)
 
 print(ipad.__str__())
 print(ipad_mini.__str__())
 print(ariston.__str__())
 print(smeg.__str__())
-print(f'The average price of all items is {Item.avg_price()}')
 
-print(ipad.price is ariston.price)
-print(ipad.display is ipad_mini.display)
-print(smeg.capacity is ariston.capacity)
+print(Tablet.avg_price(Tablet))
+print(Fridge.avg_price(Fridge))
+print(Item.avg_price(Item))
+
+# print(ipad.price is ariston.price)
+# print(ipad.display is ipad_mini.display)
+# print(smeg.capacity is ariston.capacity)
+
